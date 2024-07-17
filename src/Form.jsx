78be04614e4e83dc4848a15c4b33bd9f78a77dcb
suheila-77 <zuheilaupdalla7@gmail.com>
2024-07-17 09:39:03 +0300

    form validation

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,36 +3,50 @@ import React, { useState } from 'react';
 const Form = ({ isClose, handle }) => {
   const [close, setClose] = useState(false);
 
+  const [title, setTitle] = useState("")
+  const[description, setDescription]= useState("")
+  const[error, setError] = useState("false")
+
   const handleForm = () => {
-    setClose(true);
+    event.preventDefault()
+   if(title == "" && description== ""){
+    setError(true)
+
+    setTimeout(()=>{
+        setError(false)
+    },2000)
+   }
   };
 
   return (
     <div
       style={{ display: isClose === false ? 'none' : 'block' }}
-      className='bg-black h-[800px]  absolute w-full top-0'>
+      className='bg-black h-full absolute w-full top-0'>
     
-      <div className='flex justify-center pt-10'>
-        <form className='w-full sm:w-[500px] rounded-lg h-auto sm:h-[650px] bg-white mt-4 sm:mt-0 ml-4 sm:ml-0 p-4'>
-          <i onClick={handle} className='fa-solid fa-x text-3xl ml-[400px]'></i>
+      <div className='flex justify-center items-center h-full'>
+        <form className='w-full sm:w-[450px] rounded-lg h-auto sm:h-[600px] bg-white mt-4 sm:mt-0 ml-4 sm:ml-0 p-4 relative'>
+          <i onClick={handle} className='fa-solid fa-x text-3xl absolute top-4 right-10 cursor-pointer'></i>
+
+          {
+     error == true ? <p className=' text-red-500 text-3xl'>please fill the form</p> : ""
+     
+          }
           <br />
-          <input
-            className='w-full sm:w-[420px] h-[50px] mb-5 px-4 py-2 border-2 border-black rounded-md ml-2 mt-4'
-            type='text'
-            placeholder='Enter your name'
+          <br />
+          <input value={title} onChange={(event)=>
+            setTitle(event.target.value)
+          } 
+          className='w-full sm:w-[400px] h-[50px] mb-5 px-4 py-2 border-2 border-black rounded-md mt-4' type='text' placeholder='Enter your name'
           />
-          <input
-            className='w-full sm:w-[420px] h-[50px] mb-5 px-4 py-2 border-2 border-black rounded-md ml-2'
-            type='password'
-            placeholder='Enter your password'
-          />
-          <textarea
-            className='w-full sm:w-[400px] h-[300px] ml-[10px] resize-none'
-            placeholder='Enter title'
-          ></textarea>
-          <button className='bg-thirdColor w-full sm:w-[400px] text-3xl mt-5 ml-2 p-2'>
-            Submit
-          </button>
+         
+          <textarea value={description} 
+          onChange={(event)=>
+            setDescription(event.target.value)
+          }
+          className='w-full sm:w-[400px] h-[200px] sm:h-[300px]
+           resize-none border-2 border-black rounded-md px-4 py-2 mb-5' rows={6}  placeholder='Enter title'></textarea>
+          <button onClick={handleForm} className='bg-thirdColor w-full sm:w-[400px] text-xl sm:text-3xl mt-3 p-2 rounded-md'>  Submit</button>
+         
         </form>
       </div>
     </div>
